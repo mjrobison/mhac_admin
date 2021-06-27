8500b84014791cd74d60ca20b44586a151242aa6
Matt Robison <mrobison@flogistix.com>
2021-06-27 00:18:25 -0500

    Getting ready to release the admin

--- mhac_admin/settings/production.py
+++ mhac_admin/settings/production.py
@@ -1,4 +1,6 @@
 from .base import *
+from configparser import ConfigParser
+
 
 DEBUG = False
 
@@ -6,3 +8,25 @@ try:
     from .local import *
 except ImportError:
     pass
+
+parser = configparser.ConfigParser()
+parser.read("~/db.conf")
+
+SECRET_KEY = parser.get("django", "secrets")
+USER = parser.get("DB", "user")
+NAME = parser.get("DB", "name")
+PASSWORD = parser.get("DB", "password")
+HOST = parser.get("DB", "host")
+PORT = parser.get("DB", 'port')
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
+        'PORT': PORT,
+    }
+}
