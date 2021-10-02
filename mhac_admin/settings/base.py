d2b87e292cb2877be2a20a504df485801eadf340
Matt Robison <mrobison@flogistix.com>
2021-10-02 11:49:49 -0500

    Docker updates

--- mhac_admin/settings/base.py
+++ mhac_admin/settings/base.py
@@ -111,7 +111,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
+        'HOST': 'host.docker.internal',
         'PORT': '5432',
     }
 }
--- streams/blocks.py
+++ streams/blocks.py
@@ -22,10 +22,21 @@ class ParagraphRichTextBlock(blocks.StructBlock):
 
 
 class ContentImageBlock(blocks.StructBlock):
-    seasons = requests.get('http://localhost:8000/getSeasons')
-    seasons = seasons.json()
-    # print(seasons)
-    seasons = [(season['season_id'], season['season_name'] + ' ' + season['level']) for season in seasons]
+    try:
+        seasons = requests.get('http://localhost:8000/getSeasons')
+        seasons = seasons.json()
+        print(seasons)
+    except Exception as exc:
+        seasons = [{
+            'season_id': '',
+            'season_name': '',
+            'level': {
+                'name': '',
+                'id': ''
+            }
+        }]
+
+    seasons = [(season['season_id'], season['season_name'] + ' ' + season['level']['name']) for season in seasons]
     # print(seasons)
     year = blocks.ChoiceBlock(choices=seasons) # CharBlock(max_length=4, null=True, blank=True)
     image = ImageChooserBlock(Required=True)
