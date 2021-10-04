ed8432def93c1f4e4d639583aa1dc6f10520c9f7
Matt Robison <mrobison@flogistix.com>
2021-10-04 10:56:19 -0500

    Resstructure how the seasons call is build in the content image block. To handle null response.

--- streams/blocks.py
+++ streams/blocks.py
@@ -26,6 +26,7 @@ class ContentImageBlock(blocks.StructBlock):
         seasons = requests.get('http://localhost:8000/getSeasons')
         seasons = seasons.json()
         print(seasons)
+        seasons = [(season['season_id'], season['season_name'] + ' ' + season['level']['name']) for season in seasons]
     except Exception as exc:
         seasons = [{
             'season_id': '',
@@ -36,8 +37,6 @@ class ContentImageBlock(blocks.StructBlock):
             }
         }]
 
-    seasons = [(season['season_id'], season['season_name'] + ' ' + season['level']['name']) for season in seasons]
-    # print(seasons)
     year = blocks.ChoiceBlock(choices=seasons) # CharBlock(max_length=4, null=True, blank=True)
     image = ImageChooserBlock(Required=True)
     image_location = blocks.ChoiceBlock(choices=[
