68b3c5a692c9d44e40329ee53b51c3a7cc0fd6d5
Matt Robison <mrobison@flogistix.com>
2022-01-17 00:35:25 -0600

    Trying to get docker working

--- mhac_admin/settings/base.py
+++ mhac_admin/settings/base.py
@@ -190,7 +190,7 @@ WAGTAIL_SITE_NAME = "mhac_admin"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://mhacsports.com'
+BASE_URL = 'http:/localhost:8080'
 
 SITE_ID = 1
 
